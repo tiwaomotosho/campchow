5517ebef2419b654242b17f86d77af8ac4ab253f
Tiwalade Omotosho <tiwababa194@gmail.com>
2026-06-14 11:06:36 +0100

    fix failure with vercel compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,6 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
-    preset: "vercel",
+    preset: "vercel-edge",
   },
 });
